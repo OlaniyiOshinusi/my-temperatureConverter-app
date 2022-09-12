40da5b111c7fd4490952d83d367c9437a611f1a7
OlaniyiOshinusi <90407109+OlaniyiOshinusi@users.noreply.github.com>
2022-09-12 19:08:24 +0100

    Renders h1

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders H1', () => {
   render(<App />);
   const h1Element = screen.getByText(/temperature converter/i);
   expect(h1Element).toBeInTheDocument();
